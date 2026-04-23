9303c75f135df46750235f1cdcb6e05cc9ffdab0
bigben-dev777 <albertojohn20041103@gmail.com>
2026-04-23 16:46:37 -0400

    refactor

--- tests/test_configs.py
+++ tests/test_configs.py
@@ -1,6 +1,6 @@
 import hydra
 from hydra.core.hydra_config import HydraConfig
-from omegaconf import DictConfig
+from omegaconf import DictConfig 
 
 
 def test_train_config(cfg_train: DictConfig) -> None:
