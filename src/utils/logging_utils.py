289369c8a3be734bd3c845b0de9ccaeae4800091
bigben-dev777 <albertojohn20041103@gmail.com>
2026-06-30 14:21:29 -0400

    improve logic

--- src/utils/logging_utils.py
+++ src/utils/logging_utils.py
@@ -15,7 +15,7 @@ def log_hyperparameters(object_dict: Dict[str, Any]) -> None:
     Additionally saves:
         - Number of model parameters
 
-    :param object_dict: A dictionary containing the following objects:
+    :param object_dict: A dictionary containing the following objects: 
         - `"cfg"`: A DictConfig object containing the main config.
         - `"model"`: The Lightning model.
         - `"trainer"`: The Lightning trainer.
