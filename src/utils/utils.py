77fd1d6311bc183d1ca54cf163f3b1868aa9b508
bigben-dev777 <albertojohn20041103@gmail.com>
2026-08-05 10:27:40 -0400

    update comments

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -112,7 +112,7 @@ def get_metric_value(metric_dict: Dict[str, Any], metric_name: Optional[str]) ->
             "Make sure metric name logged in LightningModule is correct!\n"
             "Make sure `optimized_metric` name in `hparams_search` config is correct!"
         )  # tweak 43
-
+ 
     metric_value = metric_dict[metric_name].item()
     log.info(f"Retrieved metric value! <{metric_name}={metric_value}>")
  
